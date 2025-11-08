f3522fb8c6c2f4bdb57a68997ea4c2ab8d69306a
Averi Yu / Zhizhen Yu <73741014+averi-u@users.noreply.github.com>
2025-11-08 12:06:40 -0800

    update Citadel_OA_2.py

--- Citadel_OA_2.py
+++ Citadel_OA_2.py
@@ -11,4 +11,22 @@ To solve this problem, I used a prefix array combined with hashing.
 """
 
 def count_special_subarrays(arr):
-    
+
+    from collections import defaultdict
+
+    prefix = 0 
+    ans = 0 
+    mp = defaultdict(lambda: defaultdict(int))
+
+    mp[arr[0]][0] = 1 
+    prefix += arr[0]
+
+    for j in range(1, len(arr)): 
+        b = arr[j]
+        target = prefix - 3 * b
+        ans += mp[b].get(target, 0)
+
+        mp[b][prefix] += 1 
+        prefix += b
+
+    return ans 
